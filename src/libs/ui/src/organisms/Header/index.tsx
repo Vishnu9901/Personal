ab8f1931a7421127a9a907f88f5cfacc9711b244
Vishnu9901 <vishnuvardhan.vemula@techdenali.com>
2024-10-04 17:53:44 +0530

    header changes

--- src/libs/ui/src/organisms/Header/index.tsx
+++ src/libs/ui/src/organisms/Header/index.tsx
@@ -3,7 +3,7 @@ import './header.styles.scss';
 import logo from '../../../assets/Logo.svg';
 import searchIcon from '../../../assets/searchIcon.svg';
 import profile from '../../../assets/profile.svg';
-import cart from '../../../assets/cart.svg'
+import cart from '../../../assets/cart-wheels.svg'
 import { TopHeader } from '../TopHeader';
 import { NavigationContainer } from '../../molecules/HeaderNavigationContainer';
 import sell from '../../../assets/sell.svg';
@@ -19,7 +19,7 @@ export const Header = () => {
                 </div>
             </TopHeader>
             <header className='shadow-bottom'>
-                <div className="container grid pl-6 pr-6 headerContainer lg:pl-appPaddingLeft lg:pr-appPaddingRight items-center">
+                <div className="container pl-6 pr-6 headerContainer lg:pl-appPaddingLeft lg:pr-appPaddingRight items-center">
                     <div className='p-4 max-xsm:p-3'>
                         <Image src={logo} alt='logo' className='lg:flex hidden'></Image>
                         <Button className='lg:hidden flex'>
@@ -31,20 +31,27 @@ export const Header = () => {
                         <NavigationContainer></NavigationContainer>
                     </div>
 
-                    <div className='flex justify-end  lg:gap-8'>
+                    <div className='hidden lg:flex justify-end lg:gap-8'>
+                        <Button className='headerContainer-icon max-xsm:p-1'>
+                            <Image src={searchIcon} alt='searchIcon'></Image>
+                        </Button>
+                        <Button className='headerContainer-icon lg:flex hidden'>
+                            <Image src={profile} alt='profile'></Image>
+                        </Button>
+                        <Button className='headerContainer-icon max-xsm:p-1'>
+                            <Image src={cart} alt='cart'></Image>
+                        </Button>
+                    </div>
+                    <div className='lg:hidden flex justify-end lg:gap-8'>
                         <Image src={logo} alt='logo' className='lg:hidden flex'></Image>
                         <div className='flex'>
-                            <button className='headerContainer-icon max-xsm:p-1'>
+                            <Button className='headerContainer-icon max-xsm:p-1'>
                                 <Image src={searchIcon} alt='searchIcon'></Image>
-                            </button>
-                            <button className='headerContainer-icon lg:flex hidden'>
-                                <Image src={profile} alt='profile'></Image>
-                            </button>
-                            <button className='headerContainer-icon max-xsm:p-1'>
+                            </Button>
+                            <Button className='headerContainer-icon max-xsm:p-1'>
                                 <Image src={cart} alt='cart'></Image>
-                            </button>
+                            </Button>
                         </div>
-
                     </div>
                 </div>
             </header>
