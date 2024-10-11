770c59a8848bffbcb5dfaaad3be98c4e4f0df41e
Vishnu9901 <vishnuvardhan.vemula@techdenali.com>
2024-10-11 18:58:04 +0530

    git commit login modal and app spinner changes

--- src/libs/ui/src/organisms/LoginModal/index.tsx
+++ src/libs/ui/src/organisms/LoginModal/index.tsx
@@ -1,23 +1,33 @@
 import Modal from "@ui/molecules/Modal"
 import ModalHeader from "@ui/molecules/ModalHeader"
 import LoginForm from "../LoginForm"
+import { useState } from "react";
+import ForgotPasswordForm from "../ForgotPasswordForm";
 
-
+enum LoginModalConstant {
+    Login = 'login',
+    ForgotPassword = 'ForgotPassword',
+    AlreadyRegistered = 'alreadyRegistered'
+}
 interface LoginModalProps {
     close: () => void,
 }
 export const LoginModal: React.FC<LoginModalProps> = ({ close }) => {
+    const [currentForm, setCurrentForm] = useState<LoginModalConstant.Login | LoginModalConstant.ForgotPassword | LoginModalConstant.AlreadyRegistered>(LoginModalConstant.Login);
     const onSubmit = () => {
         console.log('Login')
     }
-    const forgotPassword = () =>{
-        console.log('forgotpassword')
+    const forgotPassword = () => {
+        console.log('forgotpassword');
+        setCurrentForm(LoginModalConstant.ForgotPassword)
     }
     return (
         <>
             <Modal>
                 <ModalHeader onClose={close}></ModalHeader>
-                <LoginForm onSubmit={onSubmit} forgotPassword={forgotPassword}></LoginForm>
+                {currentForm === LoginModalConstant.Login && <LoginForm onSubmit={onSubmit} forgotPassword={forgotPassword}></LoginForm>}
+                {currentForm === LoginModalConstant.ForgotPassword && <ForgotPasswordForm onSubmit={onSubmit} logiModal={()=> setCurrentForm(LoginModalConstant.Login)}></ForgotPasswordForm>}
+                {currentForm === LoginModalConstant.AlreadyRegistered && <ForgotPasswordForm onSubmit={onSubmit} logiModal={()=> setCurrentForm(LoginModalConstant.Login)}></ForgotPasswordForm>}
             </Modal>
         </>
     )
--- src/routes/routest.tsx
+++ src/routes/routest.tsx
@@ -2,12 +2,17 @@ import { Route, Routes } from 'react-router-dom'
 import { TestPage } from '../pages/TestPage/TestPage'
 import { LandingPage } from '../pages/LandingPage'
 import { MainModule } from '../modules/mainModule/mainModule'
+import { Suspense } from 'react'
+import { AppSpinner } from '@ui/atoms/AppSpinner'
 
 export const AppRoutes = () => {
   return (
     <>
       <Routes>
-        <Route path="" element={<MainModule></MainModule>}>
+        <Route path="" element={
+          <Suspense fallback={<AppSpinner />}>
+            <MainModule></MainModule>
+          </Suspense>}>
           <Route path="test" element={<TestPage></TestPage>} />
           <Route path="" element={<LandingPage></LandingPage>} />
         </Route>
